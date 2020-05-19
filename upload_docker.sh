a583e26e6c25669c9b2ebb186ca6491c1ce8548d
EC2 Default User <ec2-user@ip-10-0-7-224.us-west-2.compute.internal>
2020-05-19 03:44:09 +0000

    feat: Docker Flask Application to Predict Home Prices Complete

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=deitryn/sb_05-2020
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username deitryn
+docker tag deitryn/sb_05-2020 deitryn/sb_05-2020[:predictive_home_pricer]
 
 # Step 3:
 # Push image to a docker repository
+docker push deitryn/sb_05-2020
